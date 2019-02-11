0ae350c091924676315d7bd2d4d9fc926fa37ae9
DiegoBaluganti <balugantidiego@gmail.com>
2019-02-11 15:27:40 +0100

    Dropdown list for actor gender in Create mode. first draft.

---
+++ Models/Actor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace MvcMovie.Models {
+    public enum Gender {
+        Male,
+        Female,
+        Unknown
+    }
+
+    public class Actor {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public Gender ActorGender { get; set; } // richiamo per dropdownlist
+    }
+}
