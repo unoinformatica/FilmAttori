5802eb6b6660e12166bf924edfc4533368331d1f
DiegoBaluganti <balugantidiego@gmail.com>
2019-02-11 10:36:07 +0100

    Controller per Claudio.

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -5,11 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
-
-namespace MvcMovie.Controllers
-{
-    public class ActorsController : Controller
-    {
+//Ciao Cla
+namespace MvcMovie.Controllers {
+    public class ActorsController : Controller {
         private readonly MvcMovieContext _context;
 
         public ActorsController(MvcMovieContext context) {
@@ -31,5 +29,51 @@ namespace MvcMovie.Controllers
 
             return View(actorVM);
         }
+        // GET: Actors/Create
+        public IActionResult Create() {
+            return View(new Actor {
+                FullName = "",
+                BirthDate = DateTime.Now,
+                ActorGender = Gender.Unknown
+            }
+            );
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,FullName,BirthDate,ActorGender")] Actor actor) {
+            if (ModelState.IsValid) {
+                _context.Add(actor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(actor);
+        }
+
+        // GET: Actors/Delete/5
+        public async Task<IActionResult> Delete(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            var actor = await _context.Actors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (actor == null) {
+                return NotFound();
+            }
+
+            return View(actor);
+        }
+
+        // POST: Actors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id) {
+            var actor = await _context.Actors.FindAsync(id);
+            _context.Actors.Remove(actor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
-} 
+}
